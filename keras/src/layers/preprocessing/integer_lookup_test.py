abea9bc825d4ddb95e223f350fb5b99887fda565
BingjieZhao <493593261@qq.com>
2025-02-21 11:10:33 +0100

    test: added a new test case for set_vocabulary_special_tokens (#17)

--- keras/src/layers/preprocessing/integer_lookup_test.py
+++ keras/src/layers/preprocessing/integer_lookup_test.py
@@ -115,6 +115,11 @@ class IntegerLookupTest(testing.TestCase):
         with self.assertRaises(ValueError):
             layer.set_vocabulary([1, 2, 2, 4])
 
+    def test_set_vocabulary_special_tokens(self):
+        layer = layers.IntegerLookup(output_mode="int", mask_token=0, oov_token=1)
+        layer.set_vocabulary([0, 1, 2, 3, 4])
+        self.assertEqual(layer.get_vocabulary(), [0, 1, 2, 3, 4])
+
     def test_set_vocabulary_zzprint_coverage(self):
         print("Branch Coverage Information:")
         for branch_id, flag in index_lookup.branch_flags.items():
